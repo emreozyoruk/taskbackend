6a43bffe0fed7c848bf52fda8488df6a0d8480e7
Emre <72655616+emreozyoruk@users.noreply.github.com>
2024-11-23 22:07:29 +0300

    updateFrontendLink

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React frontend adresi
+    allow_origins=["http://localhost:3000","https://taskfrontend-swart.vercel.app"],  # React frontend adresi
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
